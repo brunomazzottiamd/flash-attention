fix: ref uses scaling + added ENV VAR to enable/disable quantization scaling

flash_attn/flash_attn_triton_amd/fwd_prefill.py
@@ -1,7 +1,7 @@
 import torch
 import triton
 import triton.language as tl
-from .utils import DEBUG, DROPOUT_USE_PYTORCH, DROPOUT_DUMP, AUTOTUNE, get_shape_from_layout, get_strides_from_layout, is_cdna, is_rdna, write_dropout_mask, create_dropout_mask, create_scale_tensors
+from .utils import DEBUG, DROPOUT_USE_PYTORCH, DROPOUT_DUMP, AUTOTUNE, get_shape_from_layout, get_strides_from_layout, is_cdna, is_rdna, write_dropout_mask, create_dropout_mask, create_scale_tensors, check_is_fp8
 
 # NOTE: triton fails to import tl.constexprs so create them here for the file
 tl_DROPOUT_USE_PYTORCH: tl.constexpr = DROPOUT_USE_PYTORCH
@@ -183,9 +183,9 @@ def _attn_fwd_inner(acc, l_i, m_i, q, k_ptrs, v_ptrs, bias_ptrs, stride_kn, stri
         if IS_FP8:
             p_scale = 1
             p_scaled = (p / p_scale)
-            acc += tl.dot(p.to(v.type.element_ty), (v*v_scale).to(v.type.element_ty))
+            acc += tl.dot(p.to(v.type.element_ty), (v*v_scale).to(v.type.element_ty)) # if you want to use p_scaled: tl.dot(p_scaled.to(v.type.element_ty), v.to(v.type.element_ty)) * v_scale * p_scale
         else:
-            acc += tl.dot(p.to(v.type.element_ty), v)
+            acc += tl.dot(p.to(v.type.element_ty), v).to(tl.float32) # NOTE: acc += tl.dot(p.to(tl.float16), v.to(tl.float16)) PASSES
 
         
         k_ptrs += BLOCK_N * stride_kn
@@ -568,13 +568,7 @@ def attention_prefill_forward_triton_impl(
                                         return_softmax,
                                         use_exp2):
     
-    fp8_types = {
-        torch.float8_e4m3fnuz,
-        torch.float8_e4m3fn,  
-        torch.float8_e5m2,
-        torch.float8_e5m2fnuz,
-    }
-    is_fp8 = q.dtype in fp8_types
+    is_fp8 = check_is_fp8(q)
     
     # if qkv are fp8, then find scaling factor for quantization
     q_scale, k_scale, v_scale = create_scale_tensors(q, k, v, SCALE_PER_HEAD=True, layout=layout) # TODO: if SCALE_PER_HEAD: within the kernel itself just compute qkv_scale = tl.max(q or k or v)

flash_attn/flash_attn_triton_amd/utils.py
@@ -11,6 +11,7 @@ import triton.language as tl
 AUTOTUNE = os.environ.get('FLASH_ATTENTION_TRITON_AMD_AUTOTUNE', '0').lower() in ('1', 'true', 'yes')
 DEBUG = os.environ.get('FLASH_ATTENTION_TRITON_AMD_DEBUG', '0').lower() in ('1', 'true', 'yes')
 PERF = os.environ.get('FLASH_ATTENTION_TRITON_AMD_PERF', '0').lower() in ('1', 'true', 'yes')
+REMOVE_QUANTIZATION_SCALING = os.environ.get('FLASH_ATTENTION_TRITON_AMD_REMOVE_QUANT_SCALE', '0').lower() in ('1', 'true', 'yes')
 USE_TRITON_ROCM = os.getenv("FLASH_ATTENTION_TRITON_AMD_ENABLE", "FALSE") == "TRUE"
 if USE_TRITON_ROCM: # TODO remove this
     random.seed(42)
@@ -341,6 +342,18 @@ def is_cdna():
 def is_rdna():
     return is_hip() and get_arch() in ("gfx1030", "gfx1100", "gfx1101", "gfx1102", "gfx1200", "gfx1201")
 
+def check_is_fp8(x: torch.Tensor):
+    if REMOVE_QUANTIZATION_SCALING:
+        return False # makes all methods believe they aren't working with fp8s, so no scaling is applied
+    
+    fp8_types = {
+        torch.float8_e4m3fnuz,
+        torch.float8_e4m3fn,  
+        torch.float8_e5m2,
+        torch.float8_e5m2fnuz,
+    }
+    return x.dtype in fp8_types
+
 def create_scale_tensors(q, k, v, SCALE_PER_HEAD=False, layout='bshd'):
     """
     Create scale tensors for q and k based on the scaling configuration.
@@ -354,14 +367,7 @@ def create_scale_tensors(q, k, v, SCALE_PER_HEAD=False, layout='bshd'):
     Returns:
     tuple: (q_scale, k_scale, v_scale) tensors
     """
-    fp8_types = {
-        torch.float8_e4m3fnuz,
-        torch.float8_e4m3fn,  
-        torch.float8_e5m2,
-        torch.float8_e5m2fnuz,
-    }
-    is_fp8 = q.dtype in fp8_types
-    is_fp8 = False
+    is_fp8 = check_is_fp8(q)
 
     if layout == 'bhsd':
         seqlen_loc = 2